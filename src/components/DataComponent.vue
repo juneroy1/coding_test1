8ce899f3dd4b7be6624e00ef2ff4843fdb2d8a12
Juneroy Quinimon <eroyjune@gmail.com>
2024-04-30 17:03:53 +0800

    changed time format and include comments detaisl

--- src/components/DataComponent.vue
+++ src/components/DataComponent.vue
@@ -7,26 +7,41 @@
         <div @click="editNodeSelected">edit</div>
       </div>
     </div>
+    <div class="m-10 p-5 flex flex-col justify-between items-start">
+      <template
+        :key="indexComment"
+        v-for="(comment, indexComment) in info.comments"
+      >
+        <p>
+          by {{ comment.by }}
+          <DateFormat :dateTime="comment.date" />
+        </p>
+        <div class="flex justify-between items-center pb-5">
+          <p>{{ comment.comment }}</p>
+        </div>
+      </template>
+    </div>
   </div>
 </template>
 
 <script setup>
-// Define the custom event
-const emit = defineEmits(['deleteNode', 'editNode']);
+import DateFormat from "./DateFormat.vue";
 
+// Define the custom event
+const emit = defineEmits(["deleteNode", "editNode"]);
 
 const props = defineProps({
-    info: Object,
-    keyIndex: Number,
-})
+  info: Object,
+  keyIndex: Number,
+});
 
 const deleteNodeSelected = () => {
-    // Emit the delete node event with the current count
-    emit('deleteNode', {info: props.info});
-}
+  // Emit the delete node event with the current count
+  emit("deleteNode", { info: props.info });
+};
 
 const editNodeSelected = () => {
-    // Emit the edit node event with the current count
-    emit('editNode',  {info: props.info});
-}
+  // Emit the edit node event with the current count
+  emit("editNode", { info: props.info });
+};
 </script>
---
+++ src/components/DateFormat.vue
@@ -0,0 +1,16 @@
+<template>
+<span class="text-gray-500"> {{ formattedDate}}</span>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+import { format, parseISO } from 'date-fns';
+const props = defineProps({
+  dateTime: String,
+});
+
+const formattedDate = computed(() => {
+  const date = parseISO(props.dateTime);
+  return format(date, 'EEEE, MMMM d, yyyy p');
+});
+</script>
